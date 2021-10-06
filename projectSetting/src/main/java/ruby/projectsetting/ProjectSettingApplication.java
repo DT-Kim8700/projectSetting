3156b607bc72ab881333ef5d0f1cc9aa271d4c43
DT-Kim8700 <hiper20@naver.com>
2021-10-06 22:53:04 +0900

    프로젝트 기본 설정
    jpa, queryDsl, web, validatiom, h2, lombok

---
+++ projectSetting/src/main/java/ruby/projectsetting/ProjectSettingApplication.java
@@ -0,0 +1,13 @@
+package ruby.projectsetting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSettingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectSettingApplication.class, args);
+    }
+
+}
